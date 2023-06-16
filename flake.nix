2583cf4464c29ecedb6c26fd099a1af1c4b0cb9f
Bruno Adelé <brunoadele@gmail.com>
2023-06-16 21:17:12 +0200

    Update flakes

--- flake.nix
+++ flake.nix
@@ -7,8 +7,8 @@
 
   inputs = {
     # Nixpkgs
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.url = "github:badele/fork-nixpkgs/unstable-fix-smokeping-symbolic-links";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:badele/fork-nixpkgs/unstable-fix-smokeping-symbolic-links";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
 
