2424bd25f1bcf9a7b08ca3aca4e9bf49acfce269
Bruno Adele <brunoadele@gmail.com>
2026-03-28 11:03:38 +0100

    feat: add gitgrep user script (#59)

--- configuration/users/badele/commons.nix
+++ configuration/users/badele/commons.nix
@@ -14,7 +14,6 @@
     ../../../nix/home-manager/apps/tools.nix
     ../../../nix/home-manager/apps/editor/neovim.nix
     ../../../nix/home-manager/apps/development/packages.nix
-    ../../../nix/home-manager/apps/development/aider.nix
     ../../../nix/home-manager/apps/development/internet.nix
     ../../../nix/home-manager/apps/development/nix.nix
     ../../../nix/home-manager/apps/system/performance.nix
